6bb3b511958aeb2b6835de86267a1900f3ee151d
Skaap325 <163150420+Skaap325@users.noreply.github.com>
2026-01-11 10:46:01 +0200

    Update artist_website.py

--- artist_website.py
+++ artist_website.py
@@ -1,11 +1,1 @@
-import http.server
-import socketserver
 
-PORT = 8000
-
-# Make the server serve files from current folder (including images folder)
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Website running! Open http://localhost:{PORT}")
-    httpd.serve_forever()
